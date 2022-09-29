edf7afe75791bc67a15fe98e08909cf0d1d66cea
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2022-09-29 16:21:20 -0300

    Adicionando Exemplos
    Aula do dia 29/09/2022

---
+++ js/index.js
@@ -0,0 +1,16 @@
+import {somar,subtrair,multiplicar,divisao} from "./operacaoarimetrica.js";
+
+
+let resultado = somar(12,21);
+console.log(resultado);
+
+resultado = subtrair(21,10);
+console.log(resultado);
+
+resultado = multiplicar(2,7);
+console.log(resultado);
+
+resultado = divisao(200,10);
+console.log(resultado);
+
+
---
+++ js/operacaoarimetrica.js
@@ -0,0 +1,9 @@
+const somar = (valor1,valor2)=>valor1+valor2;
+
+const subtrair = (valor1,valor2)=> valor1-valor2;
+
+const multiplicar = (valor1,valor2)=> valor1*valor2;
+
+const divisao = (valor1,valor2)=> valor1/valor2;
+
+export {somar,subtrair,multiplicar,divisao};
